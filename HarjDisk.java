896954cfe1edea5d34938212b2bfb39e87321494
123dkb <72741489+123dkb@users.noreply.github.com>
2020-10-12 20:15:55 +0800

    Add files via upload

---
+++ HarjDisk.java
@@ -0,0 +1,16 @@
+package CP;
+
+public class HarjDisk {
+	   int amount; 
+
+	   int getAmount() {
+	      return amount;
+
+	   }
+
+	   public void setAmount(int amount) {
+	      this.amount = amount;
+
+	   }
+
+	}
